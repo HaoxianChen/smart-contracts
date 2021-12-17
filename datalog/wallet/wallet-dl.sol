d4db51c1c605f2e9995130080c727dcc84782086
Haoxian Chen <hxchen414@mgmail.com>
2021-12-17 17:44:00 -0500

    Check in progress.

--- datalog/wallet/wallet-dl.sol
+++ datalog/wallet/wallet-dl.sol
@@ -1,17 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
 enum OPCODE { EQUAL, GREATER, SMALLER, UNEQUAL }
+
+contract Util {
+    function strcmp(string memory a, string memory b) public pure returns (bool) {
+        return keccak256(bytes(a)) == keccak256(bytes(b));
+    }
+}
 
 contract Wallet {
     // States, maintaining the views
-    address owner;
-    uint private totalSupply;
-    mapping(address=>uint) private balanceOf;
+    FactsSingle owner;
+    FactsSingle totalSuply;
+    FactsMap balanceOf;
+    string[] owner_schema;
+    string[] totalSupply_schema;
+    string[] balanceOf_schema;
 
     // Auxiliary facts
     string[] mint_schema;
     FactsArray recv_mint;
 
     constructor() {
-        owner = msg.sender;
+        owner_schema.push("owner");
+        owner = new FactsSingle("owner", owner_schema);
+        Field[] memory  _fields = new Field[](1);
+        _fields[0] = Field(uint160(msg.sender));
+        owner.push(_fields);
 
         // Create tables
         mint_schema.push("sender");
@@ -21,7 +38,9 @@ contract Wallet {
     }
 
     function getOwner() public view returns (address) {
-        return owner;
+        Tuple _tuple = owner.get();
+        Field memory f = _tuple.get("owner");
+        return address(uint160(f.value));
     }
 
     // Transactions
@@ -35,19 +54,44 @@ contract Wallet {
         _fields[2] = Field(amount);
         recv_mint.push(_fields);
 
-        FactsArray commit_mint = recv_mint
-                        .where("sender", OPCODE.EQUAL, Field(uint160(owner)))
-                        .where("address", OPCODE.UNEQUAL, Field(0));
         // 2. Is the commit rule satisfy?
-        assert (commit_mint.size() > 0);
+        FactsArray commit_mint = getCommitMint(recv_mint);
+        require (commit_mint.size() > 0);
 
-        // 3. Derive view updates
+        // 3. Derive updates to the aggreates
 
-        // 3. Check properties 
+        // 4. Check properties 
+        // FactsArray add = new FactsArray("add", []);
 
-        // 4. Commit and update
+        // 5. Commit and update
         recv_mint.pop();
-        commit_mint.pop();
+    }
+
+    // function update_totalSupply(Tuple _tuple) public pure 
+    //         returns (FactsArray) {
+    //     if (_tuple.getSchema() == recv_mint.getSchema()) {
+    //         uint updateValue = _tuple.get("amount");
+    //     }
+    // }
+
+    function getCommitMint(FactsArray _recv_mint) private view
+            returns (FactsArray) {
+        string[] memory left_join_keys = new string[](1);
+        string[] memory right_join_keys = new string[](1);
+        left_join_keys[0] = "sneder";
+        right_join_keys[0] = "owner";
+        FactsArray _commit_mint = _recv_mint
+                                    .join(owner,left_join_keys,right_join_keys)
+                                    .where("address",OPCODE.UNEQUAL,Field(0));
+        return _commit_mint;
+    }
+
+    function check_overflow(FactsArray _add) 
+        private view {
+        FactsArray integer_overflow = _add.where("a",OPCODE.GREATER,Field(0))
+                                            .where("b",OPCODE.GREATER,Field(0))
+                                            .where("c",OPCODE.SMALLER,Field(0));
+        assert(integer_overflow.size() == 0);
     }
 }
 
@@ -58,9 +102,26 @@ struct Field {
 contract Tuple {
     Field[] private fields;
     string[] private schema;
+    Util util;
     constructor (string[] memory _schema, Field[] memory _fields) {
-        schema = _schema;
-        fields = _fields;
+        require(_schema.length == _fields.length);
+        for (uint i=0;i<_schema.length;i++) {
+            schema.push(_schema[i]);
+            fields.push(_fields[i]);
+        }
+
+        util = new Util();
+    }
+    function getSchema() public view returns (string[] memory) {
+        return schema;
+    }
+    function get(string memory key) public view returns (Field memory) {
+        for (uint i=0; i<schema.length;i++) {
+            if (util.strcmp(schema[i],key)) {
+                return fields[i];
+            }
+        }
+        assert(false);
     }
 }
 
@@ -73,10 +134,10 @@ abstract contract Facts {
     // interfaces
     function pushTuple(Tuple _tuple) public virtual;
     function push(Field[] memory _fields) public virtual;
-    function where(string memory _field, OPCODE op, Field memory param) 
-        pure public virtual returns (FactsArray);
-    function join(Facts that, uint join_index_left, 
-                uint join_index_right) pure public virtual returns (FactsArray);
+    // function where(string memory _field, OPCODE op, Field memory param) 
+    //     pure public virtual returns (FactsArray);
+    // function join(Facts that, uint join_index_left, 
+    //             uint join_index_right) pure public virtual returns (FactsArray);
 }
 
 contract FactsArray is Facts{
@@ -85,7 +146,9 @@ contract FactsArray is Facts{
     Tuple[] private tuples;
     constructor (string memory _name, string[] memory _schema) {
         name = _name;
-        schema = _schema;
+        for (uint i=0; i<_schema.length;i++) {
+            schema.push(_schema[i]);
+        }
     }
     function size() public view returns (uint) {
         return tuples.length;
@@ -101,31 +164,50 @@ contract FactsArray is Facts{
         tuples.pop();
     }
     function where(string memory _field, OPCODE op, Field memory param) 
-        override pure public returns (FactsArray) {
+        pure public returns (FactsArray) {
     }
-    function join(Facts that, uint join_index_left, 
-                uint join_index_right) override pure public returns (FactsArray) {
+    function join(Facts that, string[] memory join_index_left, 
+                string[] memory join_index_right) pure public returns (FactsArray) {
     }
 }
 
 contract FactsMap is Facts{
     string private name;
     string[] private schema;
-    mapping(uint256 => Tuple) private tuples;
+    mapping(uint256 => Tuple) private tupleMap;
     constructor (string memory _name, string[] memory _schema) {
         name = _name;
-        schema = _schema;
+        for (uint i=0; i<_schema.length;i++) {
+            schema.push(_schema[i]);
+        }
+    }
+    function pushTuple(Tuple _tuple) override public {
+    }
+    function push(Field[] memory _fields) override public {
+    }
+    function get(uint256 key) public view returns (Tuple) {
+        return tupleMap[key];
+    }
+}
+
+contract FactsSingle is Facts {
+    string private name;
+    string[] private schema;
+    Tuple tuple;
+
+    constructor (string memory _name, string[] memory _schema) {
+        require(_schema.length == 1);
+        name = _name;
+        for (uint i=0; i<_schema.length;i++) {
+            schema.push(_schema[i]);
+        }
     }
     function pushTuple(Tuple _tuple) override public {
     }
     function push(Field[] memory _fields) override public {
     }
     function get() public view returns (Tuple) {
-    }
-    function where(string memory _field, OPCODE op, Field memory param) 
-        override pure public returns (FactsArray) {
-    }
-    function join(Facts that, uint join_index_left, 
-                uint join_index_right) override pure public returns (FactsArray) {
+        return tuple;
     }
 }
+
