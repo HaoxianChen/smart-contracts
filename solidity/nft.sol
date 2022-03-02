5b32e77f09a9b1598d9d6fbbb284e9f730b1eb72
Haoxian Chen <hxchen414@mgmail.com>
2022-03-02 11:48:36 -0500

    Update two interface for ERC20 and ERC721.

--- solidity/nft.sol
+++ solidity/nft.sol
@@ -5,4 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract NFT is ERC721 {
     constructor() ERC721("NFT", "MCO") {
     }
+    function mint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
+
+    function burn(uint256 tokenId) public {
+        _burn(tokenId);
+    }
 }
--- solidity/token.sol
+++ solidity/token.sol
@@ -5,4 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token is ERC20 {
     constructor() ERC20("Token", "MCO") {
     }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+
 }
