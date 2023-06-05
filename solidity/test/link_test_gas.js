0d590e8431991dbd13062208ff1a57a55058f9d5
jyl-jyl <97498760+jyl-jyl@users.noreply.github.com>
2023-06-05 13:00:51 -0400

    added opcodes gas trace

--- solidity/test/link_test_gas.js
+++ solidity/test/link_test_gas.js
@@ -6,58 +6,134 @@ const {
   time, // Assertions for transactions that should fail
 } = require('@openzeppelin/test-helpers');
 
-var Link = artifacts.require("LinkToken");
+const helper = require("./helper_functions");
+const fs = require('fs');
+const path = require('path');
+
+const lowerBound = 10;
+const upperBound = 1000;
+
+const testFolder = path.join(__dirname, `../tracefiles/link`);
+// set up tests for contracts
+const testPath = path.join(testFolder, '/setup.txt');
+const setup = fs.readFileSync(testPath, 'utf-8');
+let contractName;
+let deployAccountCount = 10;
+setup.split(/\r?\n/).some(line => {
+  let lineArr = line.split(',');
+  if(lineArr[0] == 'n') {
+    contractName = lineArr[1];
+    return true;
+  }
+  if(lineArr[0] == 'a') {
+    deployAccountCount = +lineArr[1];
+  }
+})
+
+// read setup.txt in each test folder
+const transactionFolders = fs.readdirSync(testFolder, {withFileTypes: true})
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name);
+const transactionCounts = transactionFolders.length;
+var transactionName;
+var transactionFolderPath;
+var setupPath;
+var transactionCount = 1;
+var tracefileCount = 0;
+
+helper.range(transactionCounts).forEach(l => {
+  transactionName = transactionFolders[l];
+  // get the setup file
+  transactionFolderPath = path.join(testFolder, `/${transactionName}`);
+  setupPath = path.join(transactionFolderPath, '/setup.txt');
+  // get the transaction count in setup.txt for each transaction
+  if (fs.existsSync(setupPath)) {
+    console.log('setup exists');
+    const setupFS = fs.readFileSync(setupPath, 'utf-8');
+    const setupLines = setupFS.split(/\r?\n/);
+    let i = 0;
+    while(i < setupLines.length) {
+      let sl = setupLines[i];
+      if (sl.startsWith('nt')) {
+        transactionCount = +sl.split(',')[1];
+      }
+      i++;
+    }
+  }
+
+  if(transactionName == 'transfer') {
+    tracefileCount = transactionCount;
+    helper.range(tracefileCount).forEach(testFileIndex => {
+      let fileName = `${transactionName}_${testFileIndex}.txt`;
+      let constructorSenderIndex = helper.random(0, deployAccountCount);
+      let arrayRandom = [];
+      for (let appIndex = 0; appIndex < deployAccountCount; appIndex++) {
+        if(appIndex != constructorSenderIndex) {
+          arrayRandom.push(appIndex);
+        }
+      }
+      let arrayRandomLen = arrayRandom.length;
+      let transferAccountIndex = arrayRandom[helper.random(0, arrayRandomLen)];
+      let transferAmount = helper.random(lowerBound, upperBound+1);
+      let text = `transfer,constructor,,,${constructorSenderIndex},,false\ntransfer,transfer,instance,accounts[${transferAccountIndex}] ${transferAmount},${constructorSenderIndex},,true\n`;
+      fs.writeFileSync(path.join(transactionFolderPath, fileName), text, function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+      });
+    }) 
+  }
+
+  if(transactionName == 'approve') {
+    tracefileCount = transactionCount;
+    helper.range(tracefileCount).forEach(testFileIndex => {
+      let fileName = `${transactionName}_${testFileIndex}.txt`;
+      let constructorSenderIndex = helper.random(0, deployAccountCount);
+      let arrayRandom = [];
+      for (let appIndex = 0; appIndex < deployAccountCount; appIndex++) {
+        if(appIndex != constructorSenderIndex) {
+          arrayRandom.push(appIndex);
+        }
+      }
+      let arrayRandomLen = arrayRandom.length;
+      let approveAccountIndex = arrayRandom[helper.random(0, arrayRandomLen)];
+      let approveAmount = helper.random(lowerBound, upperBound+1);
+      let text = `approve,constructor,,,${constructorSenderIndex},,false\napprove,approve,instance,accounts[${approveAccountIndex}] ${approveAmount},${constructorSenderIndex},,true\n`;
+      fs.writeFileSync(path.join(transactionFolderPath, fileName), text, function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+      });
+    }) 
+  }
 
 
-contract("Link", async accounts => {
-  it("test Link.transfer gas consumption", async() => {
-    const instance = await Link.new();
-    const result = await instance.transfer(accounts[1], 100);
-    const gasUsed = await result.receipt.gasUsed;
-    console.log("Link.transfer Gas Used: ", gasUsed);
-  });  
-
-  it("test Link.approve gas consumption", async() => {
-    const instance = await Link.new();
-    const result = await instance.approve(accounts[1], 100, {from: accounts[0]});
-    const gasUsed = await result.receipt.gasUsed;
-    console.log("Link.approve Gas Used: ", gasUsed);
-  });
-
-  it("test Link.transferFrom gas consumption", async() => {
-    const instance = await Link.new();
-    // transfer 30 from accounts[0] to accounts[1]
-    await instance.transfer(accounts[1], 30);
-    // allow accounts[0] to spend 30 on behalf of accounts[1]
-    await instance.approve(accounts[0], 30, {from: accounts[1]});
-    // accounts[0] spend 30 and transfer it to accounts[0] on behalf of accounts[1]
-    const result = await instance.transferFrom(accounts[1], accounts[2], 30, {from: accounts[0]});
-    const gasUsed = await result.receipt.gasUsed;
-    console.log("Link.transferFrom Gas Used: ", gasUsed);
-  });
-
-  // tests for parent contracts (StandardToken)  
-  it("test Link.increaseApproval gas consumption", async() => {
-    const instance = await Link.new();
-    await instance.approve(accounts[1], 50, {from: accounts[0]});
-    const result = await instance.increaseApproval(accounts[1], 50, {from: accounts[0]});
-    const gasUsed = await result.receipt.gasUsed;
-    console.log("Link.increaseApproval Gas Used: ", gasUsed);
-  });  
-
-  it("test Link.decreaseApproval gas consumption", async() => {
-    const instance = await Link.new();
-    await instance.approve(accounts[1], 100, {from: accounts[0]});
-    const result = await instance.decreaseApproval(accounts[1], 50, {from: accounts[0]});
-    const gasUsed = await result.receipt.gasUsed;
-    console.log("Link.decreaseApproval Gas Used: ", gasUsed);
-  });
+  if(transactionName == 'transferFrom') {
+    tracefileCount = transactionCount;
+    helper.range(tracefileCount).forEach(testFileIndex => {
+      let fileName = `${transactionName}_${testFileIndex}.txt`;
+      let constructorSenderIndex = helper.random(0, deployAccountCount);
+      let arrayRandom = [];
+      for (let appIndex = 0; appIndex < deployAccountCount; appIndex++) {
+        if(appIndex != constructorSenderIndex) {
+          arrayRandom.push(appIndex);
+        }
+      }
+      let arrayRandomLen = arrayRandom.length;
+      let transferAccountIndex = arrayRandom[helper.random(0, arrayRandomLen)];
+      let transferAmount = helper.random(lowerBound, upperBound+1);
+      let text = `approve,constructor,,,${constructorSenderIndex},,false\napprove,approve,instance,accounts[${approveAccountIndex}] ${approveAmount},${constructorSenderIndex},,true\n`;
+      fs.writeFileSync(path.join(transactionFolderPath, fileName), text, function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+      });
+    }) 
+  }
 
 
 
+
+
+    
 
 
 })
-
-
-
+helper.runTests(transactionCounts, transactionFolders, testFolder, contractName)
